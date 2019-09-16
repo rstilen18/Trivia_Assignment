20d85dc7a8f3dd2d48696f254f3f7f11830ca286
rstilen18 <55376315+rstilen18@users.noreply.github.com>
2019-09-16 01:46:47 -0500

    Add files via upload

---
+++ Rosalyn Stilen Trivia Assignment.hyperesources/triviaInfo-5.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"BROOKLYN 99 TRIVIA";
+	window.questions = [		"WHAT IS CAPTAIN HOLT'S NICKNAME FOR KEVIN?",
+								"BETWEEN HITCHCOCK AND SCULLY, WHICH ONE IS SCULLY?",
+								"WHAT IS ROSA DIAZ'S REAL NAME?", 
+								"WHO IS PERALTA'S ARCH NEMESIS/FORBIDDEN BEST FRIEND?", 
+                                                                "WHAT IS TERRY'S FAVORITE FOOD?",
+                                                                "IN THE EPISODE 'THE BET', WHAT IS AMY'S PUNISHMENT FOR LOSING?",
+                                                                "WHAT IS GINA'S SPIRIT ANIMAL?",
+                                                                "IN WHICH SEASON DID AMY WIN THE HALLOWEEN HEIST?",
+                                                                "WHAT INCORRECT MOVIE QUOTE DID BOYLE SHOUT IN THE DIE HARD EPISODE?",
+								"WHAT IS THE NAME OF KEVIN AND CAPTAIN HOLT'S CORGI?" ];
+	window.correctAnswer = [ 	"D", "C", "A", "B", "E" , "D", "E", "A", "B", "B"]; 
+	window.answerA = [ 			"SIR", 
+								"PIZZA GUY", 
+								"YOU'LL NEVER FIND OUT.", 
+								"ADRIAN PIMENTO", 
+								"THE SLOPPY JESSICA", 
+                                                                "DRIVNG",
+                                                                "TIGERESS",
+                                                                "SEASON 3",
+                                                                "BOYLE. CHARLES BOYLE.",
+                                                                "HUBBLE"];
+	window.answerB = [ 			"HONEY", 
+								"THEY'RE BOTH SCULLY", 
+								"GLORIA MARTINEZ", 
+								"PONTIAC BANDIT",
+								"TURKEY",
+                                                                "EATING HER OWN FOOD",
+                                                                "SWAN", 
+                                                                "SEASON 6",
+                                                                "YIPPIE KAYAK MOTHERBUCKETS!",
+                                                                "CHEEDAR"];
+	window.answerC = [ 			"KEV",
+								"BOXIER HAIR",
+								"ROSA PIMENTO", 
+								"GILDHORN", 
+								"RAW PROTEIN POWDER",
+                                                                "PAPERWORK",
+                                                                "DRAGON",
+                                                                "SHE HAS YET TO WIN THE TITLE",
+                                                                "HASTA LA VISA, MANCHEGO",
+                                                                "DAISY" ];
+	window.answerD = [ 			"KEVIN",  
+								"ROUNDER ONE",
+								"ROSA DIAZ", 
+								"CHEDDAR",
+								"COCAO NIBS", 
+                                                                "THE WORST DATE EVER",
+                                                                "HORSE",
+                                                                "SEASON 1",
+                                                                "E.T. PHONE PRESINCT",
+                                                                "PATRICK"];
+	window.answerE = [ 			"DARLING", 
+								"NO ONE CAN TELL", 
+								"GINA LINETTI", 
+								"THE VULTURE", 
+								"YOGURT",
+                                                                "PAYING FOR LUNCH", 
+                                                                "WOLF",
+                                                                "SEASON 5",
+                                                                "HAPPY TRIALS, EVIL GERMAN GUY!",
+                                                                "BEETHOVEN"];
+	window.response = [ 		"KEVIN COZNER DOES NOT LIKE TO BE CALLED 'KEV'.", 
+								"'NO ONE CAN TELL' HONESTLY COULD'VE BEEN THE RIGHT ANSWER, I BINGED THE SHOW AND STILL HAD TO GOOGLE THAT", 
+								"AND IT'S BETTER THAT WAY.", 
+								"STAR CROSSED BESTIES :3", 
+								"TERRY LOOOOVES YOGURT",
+                                                                "IT DIDN'T TURN OUT SO BAD",
+                                                                "SHE ALSO IDENTIFIES AS 'THE HUMAN FORM OF THE 100 EMOJI'",
+                                                                "SHE TOTALLY EARNED THE TITLE OF 'AMAZING DETECTIVE/GENIUS'",
+                                                                "I WOULD SAY THE ACTUAL QUOTE HERE, BUT THIS WAS A CLASS PROJECT",
+                                                                "SHE IS 100% ONE OF THE BEST CHARACTERS IN THE SHOW."];
+							
+}
+						
